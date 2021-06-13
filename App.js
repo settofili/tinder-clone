0840994ad63f0e95e631200ea1b01dffff86897c
settofili <settworld@gmail.com>
2021-06-13 16:43:17 +0100

    Completed Header
    As you have completed Header move to the body and start working on the tinder cards

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+       {/* Tinder Cards */}
+         {/* Buttons below tinder cards */}
+
+           {/* Chat Screen */}
+             {/* Individual Chat Screen */}
+    </div>
+  );
+}
+
+export default App;
